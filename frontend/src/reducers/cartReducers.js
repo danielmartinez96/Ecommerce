f0947e558b850badaecb8810646996e90b72c6b9
Daniel Martinez <martinezfdaniel96@gmail.com>
2020-12-14 18:51:19 -0300

    remove from cart

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from "../constant/cartConstants"
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constant/cartConstants"
 
 export const cartReducer = (state={cartItems:[]},action)=>{
     switch(action.type){
@@ -13,7 +13,10 @@ export const cartReducer = (state={cartItems:[]},action)=>{
             }else{
                 return{...state,cartItems:[...state.cartItems,item]};
             }
+        case CART_REMOVE_ITEM:
+            return{...state,cartItems:state.cartItems.filter(x => x.product !== action.payload)};
         default:
             return state;
     }
-}
+};
+
